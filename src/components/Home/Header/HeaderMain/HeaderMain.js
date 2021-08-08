fe88970228afeb4082bc186cacbd5d9cc4011746
Habib058 <adnanshakil215@gmail.com>
2021-08-08 22:34:33 +0600

    done

--- src/components/Home/Header/HeaderMain/HeaderMain.js
+++ src/components/Home/Header/HeaderMain/HeaderMain.js
@@ -4,7 +4,7 @@ import chair from '../../../../images/chair.png'
 const HeaderMain = () => {
     return (
         <main style={{ height: '600px' }} className='row d-flex align-items-center'>
-            <div className="col-md-4 offset-md-1">
+            <div className="col-md-4 offset-md-1 align-self-center">
                 <h1 style={{ color: '#203047' }}>Your New Smile<br />Start Here</h1>
                 <p className='text-secondary'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quia quam deleniti ratione ut libero?</p>
                 <button className=" btn btn-primary">GET APPOINTMENT</button>
